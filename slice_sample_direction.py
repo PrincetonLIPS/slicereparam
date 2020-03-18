955febb91a443112cf7716cc8d75122955a9d787
davidzoltowski <david.m.zoltowski>
2020-03-18 10:27:04 -0700

    add backwards pass function, optimization

--- slice_sample_direction.py
+++ slice_sample_direction.py
@@ -8,21 +8,34 @@ from scipy.optimize import root, root_scalar
 
 import matplotlib.pyplot as plt
 
-D = 2
+from tqdm.auto import trange
+
+D = 3
 theta = np.zeros(D)
 x = 0.5 * np.ones(D)
 Sigma = np.eye(D)
-xstar = np.array([1.1,0.5])
-
-S = 4
+xstar = np.array([1.1,0.5,0.4])
+S = 3
 
 # Sigma = np.array([[1.0, 0.5],[0.5,1.0]])
 # Sigma = np.array([[1.0]])
 def log_pdf(x, theta):
     d = x.shape[0]
     mu = theta
-    normalizer = np.power(2*np.pi, d/2) * np.sqrt(np.linalg.det(Sigma))
-    return -0.5 * (x - mu).T @ np.linalg.inv(Sigma) @ (x - mu) - np.log(normalizer)
+    # normalizer = np.power(2*np.pi, d/2) * np.sqrt(np.linalg.det(Sigma))
+    return -0.5 * (x - mu).T @ np.linalg.inv(Sigma) @ (x - mu) #- np.log(normalizer)
+
+sig2 = 1.0
+def log_pdf(x, theta):
+    d = x.shape[0]
+    val = 0.0
+    val += -1.0 * np.abs(x[0] - theta[0]) / sig2
+    val += -1.0 * np.abs(x[1] - theta[1]) / sig2
+    val += -1.0 * np.abs(x[2] - theta[2]) / sig2
+    return val
+
+# def pdf(x, mu, sig2):
+#     return 1.0 / (2.0 * sig2) * np.exp(- np.abs(x - mu) / sig2)
 
 def log_pdf_theta(theta, x):
     return log_pdf(x, theta)
@@ -47,10 +60,8 @@ def forwards(theta, x, log_pdf, us, ds):
     xLs = []
     xRs = []
     alphas = []
-    for s in range(S):
 
-        # if np.mod(s, 50) == 0:
-        #     print(s)
+    for s in range(S):
 
         u1 = us[s,0]
         u2 = us[s,1]
@@ -81,48 +92,48 @@ xs, xLs, xRs, alphas = forwards(theta, x, log_pdf, us, ds)
 loss_fun = lambda x : np.mean((x - xstar)**2)
 dL_dx = grad(loss_fun)
 
-dL_dtheta = np.zeros_like(theta)
-
 # backwards pass
-for s in range(S-1, -1, -1):
+def backwards(theta, log_pdf, us, ds, xs, xLs, xRs, alphas,
+              grad_theta, grad_x, grad_x_ad, dL_dx):
 
-    print(s)
-    u1 = us[s,0]
-    u2 = us[s,1]
-    z_L = alphas[s][0]
-    z_R = alphas[s][1]
+    S = us.shape[0]
+    # assert ds.shape[0] == S == xLs.shape[0] == xRs.shape[0] == alphas.shape[0] == xs.shape[0]-1
 
-    dL_dx_s = dL_dx(xs[s+1]) / S
-    if s < S-1:
-        dL_dx_s += prev_dL_dx_s @ J_xs
+    dL_dtheta = np.zeros_like(theta)
 
-    # 
-    # L_grad_theta = -1.0 * (grad_theta(theta, xLs[s]) - grad_theta(theta, xs[s])) / np.dot(ds[s], grad_x(xLs[s], theta))
-    # R_grad_theta = -1.0 * ( grad_theta(theta, xRs[s]) - grad_theta(theta, xs[s]) ) / np.dot(ds[s], grad_x(xRs[s], theta))
-    L_grad_theta = -1.0 * (grad_theta(theta, xLs[s]) - grad_theta(theta, xs[s])) / np.dot(ds[s], grad_x_ad(xs[s], theta, z_L, ds[s]))
-    R_grad_theta = -1.0 * (grad_theta(theta, xRs[s]) - grad_theta(theta, xs[s])) / np.dot(ds[s], grad_x_ad(xs[s], theta, z_R, ds[s]))
+    for s in range(S-1, -1, -1):
 
-    #
-    dL_dtheta_s = u2 * np.dot(dL_dx_s, ds[s]) * R_grad_theta + (1-u2) * np.dot(dL_dx_s, ds[s]) * L_grad_theta
-    dL_dtheta += dL_dtheta_s
+        u1 = us[s,0]
+        u2 = us[s,1]
+        z_L = alphas[s][0]
+        z_R = alphas[s][1]
 
-    # compute second term
-    # if s < S-1:
-    #     intr = prev_dL_dx_s @ J_xs
-    #     second_term = u2 * np.dot(intr, ds[s]) * R_grad_theta + (1-u2) * np.dot(intr, ds[s]) * L_grad_theta
-    #     dL_dtheta += second_term 
+        dL_dx_s = dL_dx(xs[s+1]) / S
 
-    # compute Jacobian of dx_s  / dx_{s-1}
-    # L_grad_x = -1.0 * ( grad_x(xLs[s], theta) - grad_x(xs[s], theta) ) / np.dot(ds[s], grad_x(xLs[s], theta))
-    # R_grad_x = -1.0 * ( grad_x(xRs[s], theta) - grad_x(xs[s], theta) ) / np.dot(ds[s], grad_x(xRs[s], theta))
-    L_grad_x = -1.0 * ( grad_x_ad(xs[s], theta, z_L, ds[s]) - grad_x(xs[s], theta) ) / np.dot(ds[s], grad_x_ad(xs[s], theta, z_L, ds[s]))
-    R_grad_x = -1.0 * ( grad_x_ad(xs[s], theta, z_R, ds[s]) - grad_x(xs[s], theta) ) / np.dot(ds[s], grad_x_ad(xs[s], theta, z_R, ds[s]))
-    J_xs = np.eye(D) + u2 * np.outer(ds[s], R_grad_x) + (1-u2) * np.outer(ds[s], L_grad_x)
+        # if not final sample, accumulate loss
+        if s < S-1:
+            dL_dx_s += prev_dL_dx_s @ J_xs
 
-    prev_dL_dx_s = np.copy(dL_dx_s)
+        # compute gradients of xL and xR wrt theta
+        L_grad_theta = -1.0 * (grad_theta(theta, xLs[s]) - grad_theta(theta, xs[s])) / np.dot(ds[s], grad_x_ad(xs[s], theta, z_L, ds[s]))
+        R_grad_theta = -1.0 * (grad_theta(theta, xRs[s]) - grad_theta(theta, xs[s])) / np.dot(ds[s], grad_x_ad(xs[s], theta, z_R, ds[s]))
 
-# ARE THERE SIGN ISSUES?
-print("Implicit: ", dL_dtheta)
+        # compute gradient dL / dtheta
+        dL_dtheta_s = u2 * np.dot(dL_dx_s, ds[s]) * R_grad_theta + (1-u2) * np.dot(dL_dx_s, ds[s]) * L_grad_theta
+        dL_dtheta += dL_dtheta_s
+
+        # compute Jacobian of dx_s  / dx_{s-1}
+        L_grad_x = -1.0 * ( grad_x_ad(xs[s], theta, z_L, ds[s]) - grad_x(xs[s], theta) ) / np.dot(ds[s], grad_x_ad(xs[s], theta, z_L, ds[s]))
+        R_grad_x = -1.0 * ( grad_x_ad(xs[s], theta, z_R, ds[s]) - grad_x(xs[s], theta) ) / np.dot(ds[s], grad_x_ad(xs[s], theta, z_R, ds[s]))
+        J_xs = np.eye(D) + u2 * np.outer(ds[s], R_grad_x) + (1-u2) * np.outer(ds[s], L_grad_x)
+
+        # store previous loss
+        prev_dL_dx_s = np.copy(dL_dx_s)
+
+    return dL_dtheta
+
+dL_dtheta = backwards(theta, log_pdf, us, ds, xs, xLs, xRs, alphas,
+                      grad_theta, grad_x, grad_x_ad, dL_dx)
 
 dx = 0.001
 dthetas = np.zeros(D)
@@ -138,15 +149,54 @@ for d in range(D):
     loss2 = loss_fun(xs2[1:])
     dthetas[d] = (loss2 - loss1) / (2.0 * dx)
 
+print("Implicit: ", dL_dtheta)
 print("Numerical: ", dthetas)
 
+# use for optimization!
+xstar = np.array([1.1,-0.5,0.4])
+loss_fun = lambda x : np.mean((x - xstar)**2)
+dL_dx = grad(loss_fun)
 
-# dx = 0.001
-# theta1 = theta[0]-dx
-# theta2 = theta[0]+dx
-# xs1, xLs1, xRs1 = forwards(theta1, x, log_pdf, us, ds)
-# xs2, xLs2, xRs2 = forwards(theta2, x, log_pdf, us, ds)
-# loss1 = loss_fun(xs1[1:])
-# loss2 = loss_fun(xs2[1:])
-# dtheta1 = (loss2 - loss1) / (2.0 * dx)
-# print("Numerical: ", dtheta1)
+theta = np.zeros(D)
+thetas = []
+losses = [loss_fun(xs)]
+xs = [theta]
+S = 3
+num_iters=1000
+
+# learning rate params
+a0 = 0.5
+gam = 0.2
+
+pbar = trange(num_iters)
+pbar.set_description("Loss: {:.1f}".format(losses[0]))
+
+for i in range(num_iters):
+
+    us = npr.rand(S, 2)
+    ds = npr.randn(S, D)
+    ds = np.array([d / np.linalg.norm(d) for d in ds])
+
+    # forward pass
+    xs, xLs, xRs, alphas = forwards(theta, xs[-1], log_pdf, us, ds)
+
+    # backwards pass
+    dL_dtheta = backwards(theta, log_pdf, us, ds, xs, xLs, xRs, alphas,
+                      grad_theta, grad_x, grad_x_ad, dL_dx)
+
+    # update parameters
+    alpha_t = a0 / (1 + gam * (i+1)) # learning rate 
+    theta = theta - dL_dtheta * alpha_t
+    thetas.append(theta)
+    losses.append(loss_fun(xs[1:]))
+
+    pbar.set_description("Loss: {:.1f}".format(losses[-1]))
+    pbar.update()
+pbar.close()
+
+thetas_plot = np.array(thetas)
+plt.ion()
+plt.figure()
+for d in range(D):
+    plt.plot([0,num_iters],xstar[d]*np.ones(2),'k--')
+plt.plot(thetas_plot,'b')
